c6863774d1ab9fc9b0a93ba24bb8ca8b72a3b017
KillTheReptiles <duvan.diaz.2019@upb.edu.co>
2019-02-28 22:14:26 -0500

    Negativo -15 a -1

---
+++ negativo -15 a -1.py
@@ -0,0 +1,23 @@
+print("Comienzo")
+print()
+numero = int(input("Escriba un numero entre -15 y -1: "))
+while numero > 0:
+    print("Has escrito un numero positivo")
+    numero = int(input("Escriba un numero entre -15 y -1: "))
+
+while numero > -1:
+    print("Has escrito un numero entre -15 y -1")
+    numero = int(input("Escriba un numero entre -15 y -1: "))
+
+
+while numero < -15:
+    print("Has escrito un numero menor a -15")
+    numero = int(input("Escriba un numero entre -15 y -1: "))
+
+while numero < -15:
+    print("Has escrito un numero entre -15 y -1")
+    numero = int(input("Tu numero es negativo y esta entre -15 y -1, Gracias :D!: "))
+
+print("Tu numero es negativo y esta entre -15 y -1, Gracias :D!")
+print()
+print("Final")
